695fb2d9360e45b06440adc6fa318947b7330a3f
Tri Nguyen <tringuyen.nnt@gmail.com>
2020-01-30 16:59:39 +0100

    use pointer to update state index

--- server/src/interlocking.c
+++ server/src/interlocking.c
@@ -98,18 +98,18 @@ static int interlocking_table_resolve_indices(void) {
 		g_string_append_printf(log, "Path: ");
 		if (route->path != NULL) {
             for (int segment_index = 0; segment_index < route->path->len; ++segment_index) {
-                t_interlocking_path_segment segment = g_array_index(route->path, t_interlocking_path_segment, segment_index);
-                id = segment.id;
-                segment.bidib_state_index = bidib_get_segment_state_index(id);
+                t_interlocking_path_segment *segment = &g_array_index(route->path, t_interlocking_path_segment, segment_index);
+                id = segment->id;
+                segment->bidib_state_index = bidib_get_segment_state_index(id);
 
-                if (segment.bidib_state_index == -1) {
+                if (segment->bidib_state_index == -1) {
                     syslog_server(LOG_ERR, "Interlocking initialisation: %s not found in BiDiB state", id);
                     return -1;
                 }
 
                 g_string_append_printf(log, "%s (%d), ",
-                                       segment.id,
-                                       segment.bidib_state_index);
+                                       segment->id,
+                                       segment->bidib_state_index);
             }
 		}
 		g_string_append_printf(log, "\n");
@@ -117,18 +117,18 @@ static int interlocking_table_resolve_indices(void) {
 		g_string_append_printf(log, "Points: ");
 		if (route->points != NULL) {
             for (int point_index = 0; point_index < route->points->len; ++point_index) {
-                t_interlocking_point point = g_array_index(route->points, t_interlocking_point, point_index);
-                id = point.id;
-                point.bidib_state_index = bidib_get_point_state_index(id);
+                t_interlocking_point *point = &g_array_index(route->points, t_interlocking_point, point_index);
+                id = point->id;
+                point->bidib_state_index = bidib_get_point_state_index(id);
 
-                if (point.bidib_state_index == -1) {
+                if (point->bidib_state_index == -1) {
                     syslog_server(LOG_ERR, "Interlocking initialisation: %s not found in BiDiB state", id);
                     return -1;
                 }
 
                 g_string_append_printf(log, "%s (%d), ",
-                                       point.id,
-                                       point.bidib_state_index);
+                                       point->id,
+                                       point->bidib_state_index);
             }
 		}
 		g_string_append_printf(log, "\n");
@@ -136,18 +136,18 @@ static int interlocking_table_resolve_indices(void) {
 		g_string_append_printf(log, "Signals: ");
 		if (route->signals != NULL) {
             for (int signal_index = 0; signal_index < route->signals->len; ++signal_index) {
-                t_interlocking_signal signal = g_array_index(route->signals, t_interlocking_signal, signal_index);
-                id = signal.id;
-                signal.bidib_state_index = bidib_get_signal_state_index(id);
+                t_interlocking_signal *signal = &g_array_index(route->signals, t_interlocking_signal, signal_index);
+                id = signal->id;
+                signal->bidib_state_index = bidib_get_signal_state_index(id);
 
-                if (signal.bidib_state_index == -1) {
+                if (signal->bidib_state_index == -1) {
                     syslog_server(LOG_ERR, "Interlocking initialisation: %s not found in BiDiB state", id);
                     return -1;
                 }
 
                 g_string_append_printf(log, "%s (%d), ",
-                                       signal.id,
-                                       signal.bidib_state_index);
+                                       signal->id,
+                                       signal->bidib_state_index);
             }
 		}
 		g_string_append_printf(log, "\n");
