089905d965d71dcc9abfd681243f0183e02ecca9
Eugene <eyip002@users.noreply.github.com>
2020-02-03 16:01:43 +0100

    Update interlocking_parser.h

--- server/src/interlocking_parser.h
+++ server/src/interlocking_parser.h
@@ -32,8 +32,8 @@
 #include <stdbool.h>
 #include <glib.h>
 
-char* concat_str(const char *str1, const char *str2);
+char *concat_str(const char *str1, const char *str2);
 
-GArray* parse_interlocking_table(const char *config_dir);
+GArray *parse_interlocking_table(const char *config_dir);
 
 #endif // INTERLOCKING_PARSER_H
