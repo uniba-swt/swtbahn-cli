e30e0a92493fe3202f8e954e6a51bb95c912ceb2
Eugene Yip <eugene.yip@uni-bamberg.de>
2020-02-04 15:12:37 +0100

    ForeC program now uses condition variables instead of busy waiting for thread synchronisation. Thread cancelling no longer works within while-loops because execution stops at the pthread_cond_wait

--- server/src/dyn_containers_interface.c
+++ server/src/dyn_containers_interface.c
@@ -45,7 +45,7 @@ static pthread_t dyn_containers_actuate_thread;
 
 static t_dyn_containers_interface *dyn_containers_interface;
 #define MICROSECOND 1
-static const int let_period_us = 500000 * MICROSECOND;//50000 * MICROSECOND;	// 0.05 seconds
+static const int let_period_us = 100000 * MICROSECOND;	// 0.1 seconds
 
 static t_dyn_shm_config shm_config;
 static const int shm_permissions = (IPC_CREAT | 0666);
