f418bebd180b85820807ea3cf049ad3aa5eeb87b
Eugene Yip <eugene.yip@uni-bamberg.de>
2021-06-21 19:33:25 +0200

    HTTP errors now showing up properly with response data

--- server/src/handler_driver.c
+++ server/src/handler_driver.c
@@ -299,20 +299,22 @@ onion_connection_status handler_request_route(void *_, onion_request *req,
 				onion_response_printf(res, "%s", route_id);
 				return OCS_PROCESSED;
 			} else {
+				syslog_server(LOG_ERR, "Request: Request train route - "
+				              "train: %s route not granted",
+				              grabbed_trains[grab_id].name->str);
+				/*
 				if (strcmp(route_id, "no_routes") == 0) {
 					onion_response_printf(res, "No routes possible from %s to %s", 
 					                      data_source_name, data_destination_name);
 				} else if (strcmp(route_id, "not_grantable") == 0) {
-					onion_response_printf(res, "Route found conflicts with others");
-				} else if (strcmp(route_id, "%s", "not_clear") == 0) {
+					onion_response_printf(res, "%s", "Route found conflicts with others");
+				} else if (strcmp(route_id, "not_clear") == 0) {
 					onion_response_printf(res, "%s", "Route found has occupied tracks");
 				} else {
 					onion_response_printf(res, "%s", "Route could not be granted");
 				}
-			
-				syslog_server(LOG_ERR, "Request: Request train route - "
-				              "train: %s route not granted",
-				              grabbed_trains[grab_id].name->str);
+				*/
+
 				return OCS_NOT_IMPLEMENTED;
 			}
 		}
