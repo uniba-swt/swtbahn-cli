e74827239117d54acad03e7aeb9214be5bc0f8d5
BLuedtke <bernhard_luedtke@posteo.de>
2022-03-07 00:42:48 +0100

    minimal-working-example update, dirty logging, 2022-03-06

--- server/src/handler_upload.c
+++ server/src/handler_upload.c
@@ -29,6 +29,7 @@
 #include <onion/onion.h>
 #include <onion/shortcuts.h>
 #include <pthread.h>
+#include <sys/syslog.h>
 #include <unistd.h>
 #include <stdio.h>
 #include <limits.h>
@@ -47,7 +48,7 @@ static const char interlocker_dir[] = "interlockers";
 static const char interlocker_extensions[][5] = { "bahn" };
 static const int interlocker_extensions_count = 1;
 
-static const char verifier_url[] = "ws://localhost:8080/engineverification/";
+static const char verifier_url[] = "ws://141.13.106.29:8080/engineverification/";
 
 extern pthread_mutex_t dyn_containers_mutex;
 
@@ -114,7 +115,7 @@ struct ws_verif_data {
 static void websock_verification_callback(struct mg_connection *c, int ev, void *ev_data, void *fn_data) {
 	if (ev == MG_EV_ERROR) {
 		// On error, log error message
-		syslog_server(LOG_ERR, "Request: Upload - Websocket callback: %s", (char *) ev_data);
+		syslog_server(LOG_ERR, "Request: Upload - Websocket callback encountered error: %s", (char *) ev_data);
 	} else if (ev == MG_EV_WS_OPEN) {
 		// Read sctx model from file and send that as the message.
 		//https://stackoverflow.com/a/174743
@@ -133,6 +134,8 @@ static void websock_verification_callback(struct mg_connection *c, int ev, void 
 				
 				//Send verification request
 				if(g_fullmsg != NULL){
+					printf("Sending verification request\n");
+					syslog_server(LOG_INFO, "Request: Upload - Now sending verification request to swtbahn-verifier");
 					mg_ws_send(c, g_fullmsg->str, g_fullmsg->len, WEBSOCKET_OP_TEXT);
 				} else {
 					syslog_server(LOG_ERR, "Request: Upload - Websocket msg payload setup has gone wrong");
@@ -150,25 +153,29 @@ static void websock_verification_callback(struct mg_connection *c, int ev, void 
 		
 		//Check if the response contains that status field -> if yes, it's the verification result
 		char* isResult = strstr(wm->data.ptr, "\"status\":");
-		
+		printf("Received: %s\n", wm->data.ptr);
 		if (isResult){	
 			//Check how the verification went
 			char* ret = strstr(wm->data.ptr, "\"status\":true");
-			
+			printf("%s",wm->data.ptr);
 			if(ret){
+				printf("\nVerif: Good Result\n");
 				syslog_server(LOG_INFO, "Request: Upload - Verification successful (result positive)");
 				(*(struct ws_verif_data *) fn_data).success = true;
 			} else {
+				printf("\nVerif: Negative Result\n");
 				syslog_server(LOG_INFO, "Request: Upload - Verification not successful (result negative)");
 				(*(struct ws_verif_data *) fn_data).success = false;
 			}
 			(*(struct ws_verif_data *) fn_data).finished = true;
 		}
 	}
-	if (ev == MG_EV_ERROR || ev == MG_EV_CLOSE){
-		//If an error is encountered, or if the websocket is closed before verification can finish, 
-		// the verification can't be completed -> success false; finished true.
+	if (ev == MG_EV_ERROR){
+		//If an error is encountered, the verification can't be completed -> success false; finished true.
 		(*(struct ws_verif_data *) fn_data).success = false;
+		(*(struct ws_verif_data *) fn_data).finished = true;
+	} else if (ev == MG_EV_CLOSE) {
+		syslog_server(LOG_INFO, "Request: Upload - Now closing websocket connection to swtbahn-verifier");
 		(*(struct ws_verif_data *) fn_data).finished = true;
 	}
 }
@@ -176,9 +183,11 @@ static void websock_verification_callback(struct mg_connection *c, int ev, void 
 onion_connection_status handler_upload_engine(void *_, onion_request *req,
                                               onion_response *res) {
 	build_response_header(res);
-	if (running && ((onion_request_get_flags(req) & OR_METHODS) == OR_POST)) {
+	printf("1 - handler_upload_engine\n");
+	if (/*running &&*/ ((onion_request_get_flags(req) & OR_METHODS) == OR_POST)) {
 		const char *filename = onion_request_get_post(req, "file");
 		const char *temp_filepath = onion_request_get_file(req, "file");
+		printf("2 - handler_upload_engine\n");
 		if (filename == NULL || temp_filepath == NULL) {
 			syslog_server(LOG_ERR, "Request: Upload - engine file is invalid");
 			
@@ -186,7 +195,7 @@ onion_connection_status handler_upload_engine(void *_, onion_request *req,
 			onion_response_set_code(res, HTTP_BAD_REQUEST);
 			return OCS_PROCESSED;
 		}
-  
+		printf("3 - handler_upload_engine\n");
  		if (engine_file_exists(filename)) {
 			syslog_server(LOG_ERR, "Request: Upload - engine file already exists");
 			
@@ -194,7 +203,7 @@ onion_connection_status handler_upload_engine(void *_, onion_request *req,
 			onion_response_set_code(res, HTTP_BAD_REQUEST);
 			return OCS_PROCESSED;
 		}  
-		
+		printf("4 - handler_upload_engine\n");
 		char final_filepath[PATH_MAX + NAME_MAX];
 		snprintf(final_filepath, sizeof(final_filepath), "%s/%s", engine_dir, filename);
 		onion_shortcut_rename(temp_filepath, final_filepath);
@@ -204,21 +213,21 @@ onion_connection_status handler_upload_engine(void *_, onion_request *req,
 		//BLuedtke: Now try to contact verification server and verify engine before proceeding
 		// Should we try to compile the model first? -> discuss
 		// Has to be done over websocket connection.
+		printf("5 - handler_upload_engine\n");
 		struct mg_mgr mgr;
-		struct ws_verif_data signals;
-		signals.file_path = g_string_new(final_filepath);
+		struct ws_verif_data ws_verifData;
+		ws_verifData.file_path = g_string_new(final_filepath);
 		// Client connection
 		struct mg_connection *c;
 		// Initialise event manager
 		mg_mgr_init(&mgr);
 		// Create client
-		c = mg_ws_connect(&mgr, verifier_url, websock_verification_callback, &signals, NULL);
-		while (c && signals.finished == false) {
+		c = mg_ws_connect(&mgr, verifier_url, websock_verification_callback, &ws_verifData, NULL);
+		while (c && ws_verifData.finished == false) {
 			mg_mgr_poll(&mgr, 1000); // Wait for reply
 		}
 		if(c && !c->is_closing){
-			//If connection is not yet closing, send close. 
-			//After that, one more event poll has to be performed,
+			//If connection is not yet closing, send close. After that, one more event poll has to be performed,
 			// otherwise the close msg will not be sent. (Also, buf = reason = max length of 1 apparently??)
 			mg_ws_send(c, "0", 1, 1000);
 			mg_mgr_poll(&mgr, 1000);
@@ -226,10 +235,10 @@ onion_connection_status handler_upload_engine(void *_, onion_request *req,
 		mg_mgr_free(&mgr); // Deallocate resources
 		
 		//Free string allocated for loading model file
-		g_string_free(signals.file_path, true);
-		
+		g_string_free(ws_verifData.file_path, true);
+		printf("6 - handler_upload_engine\n");
 		//Stop upload if verification did not succeed
-		if(!signals.success){
+		if(!ws_verifData.success){
 			pthread_mutex_unlock(&dyn_containers_mutex);
 			syslog_server(LOG_ERR, "Request: Upload - engine verification failed");
 			
@@ -237,7 +246,8 @@ onion_connection_status handler_upload_engine(void *_, onion_request *req,
 			onion_response_set_code(res, HTTP_BAD_REQUEST);
 			return OCS_PROCESSED;
 		}
-										
+		printf("7 - handler_upload_engine\n");
+		
 		char filepath[sizeof(final_filepath)];
 		remove_file_extension(filepath, final_filepath, ".sctx");
 		const dynlib_status status = dynlib_compile_scchart(filepath, engine_dir);
