790e3aed22d98cb566d44742b492792719416b11
Jochen Mehlich <contact@jochenmehlich.de>
2022-09-22 13:29:57 +0200

    update text of destination  button

--- server/src/assets/game/script-game.js
+++ server/src/assets/game/script-game.js
@@ -54,6 +54,7 @@ function setDestinationButton(choice, route) {
 	// Route details are stored in the value parameter of the destination button
 	$(`#${destinationNamePrefix}${choice}`).val(JSON.stringify(route));
 	$(`#${destinationNamePrefix}${choice}`).innerHTML = JSON.stringify(route);
+	$(`#${destinationNamePrefix}${choice}`).innerText = JSON.stringify(route);
 	$(`#${destinationNamePrefix}${choice}`).addClass(destinationEnabledButtonStyle);
 	$(`#${destinationNamePrefix}${choice}`).removeClass(destinationDisabledButtonStyle);
 }
