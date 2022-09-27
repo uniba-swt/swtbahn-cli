88493ac23ca2957e5e1fe408d386e041287d2bff
Jochen Mehlich <contact@jochenmehlich.de>
2022-09-27 14:15:21 +0200

    just append child when button doesnt have child nodes

--- server/src/assets/game/script-game.js
+++ server/src/assets/game/script-game.js
@@ -91,6 +91,7 @@ function setDestinationButton(choice, route) {
 
 	//#DiceAdd
 	if(!$(`#${destinationNamePrefix}${choice}`)[0].hasChildNodes()){
+		console.log(signalToFlagFull[destinationSignal]);
 		const diceObject = addDice(signalToFlagFull[destinationSignal]["fillColor"], signalToFlagFull[destinationSignal]["backgroundColor"], signalToFlagFull[destinationSignal]["number"], signalToFlagFull[destinationSignal]["numberClass"]);
 		$(`#${destinationNamePrefix}${choice}`)[0].appendChild(diceObject);
 	}
