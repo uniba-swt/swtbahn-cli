e08470ff360f1eb7fcdbd875fede15b44570084e
Jochen Mehlich <contact@jochenmehlich.de>
2022-10-05 17:21:00 +0200

    image handling (top) changed

--- server/src/assets/game/script-game.js
+++ server/src/assets/game/script-game.js
@@ -57,7 +57,7 @@ function setDestinationButtonUnavailable(choice, route) {
 }
 
 function showTrainHeader(trainId){
-	$('#trainheader').setAttribute("style", "");
+	$('#trainheader')[0]  .setAttribute("style", "");
 	console.log(trainId);
 	switch(trainId){
 		case "cargo_db":
