474005f094ca6514a3a5ed5c635c266e70311f8c
Bernhard Luedtke <bernhard_luedtke@posteo.de>
2022-11-18 15:20:03 +0100

    Fix debug logging

--- server/src/handler_driver.c
+++ server/src/handler_driver.c
@@ -352,10 +352,6 @@ long long get_train_pos_index_in_route_path(const char *train_id,  const t_inter
 			}
 		}
 	}
-	if (train_pos_index > 0) {
-		const char *path_item = g_array_index(route->path, char *, train_pos_index_unsigned);
-		syslog_server(LOG_NOTICE, "Train Position - Segment %s", path_item);
-	}
 	bidib_free_train_position_query(train_position_query);
 	return train_pos_index;
 }
@@ -415,12 +411,11 @@ static bool drive_route_progressive_stop_signals_decoupled(const char *train_id,
 				if (sig_info_elem != NULL && !(sig_info_elem->has_been_set_to_stop) && !(sig_info_elem->is_destination_signal)) {
 					// <= to catch source signal that has index 0 even though it does not appear in route->path.
 					if (sig_info_elem->index_in_route_path <= train_pos_index) {
-						size_t unsigned_pos = (train_pos_index > 0) ? (size_t) train_pos_index : 0;
-						char *path_item = &g_array_index(route->path, char, unsigned_pos);
-						syslog_server(LOG_NOTICE, "Drive-Route Decoupled: Set %s with index %d to stop, "
-						              "train index %d, segment %s, route->path len %d, uPos %d", sig_info_elem->id, 
-										  sig_info_elem->index_in_route_path, train_pos_index, 
-										  path_item, route->path->len, unsigned_pos);
+						if (train_pos_index > 0) {
+							const char *path_item = g_array_index(route->path, char*, train_pos_index);
+							syslog_server(LOG_NOTICE, "Drive-Route Decoupled: route->path item %s reached, thus set signal %s to stop",
+							              path_item, sig_info_elem->id);
+						}
 						if (bidib_set_signal(sig_info_elem->id, signal_stop_aspect)) {
 							// Log_ERR... but what else? Safety violation once we have a safety layer?
 							syslog_server(LOG_ERR, "Drive-Route Decoupled: unable "
