7e88ac19258d8b18998bc00d33c416c6f30b1731
Jochen Mehlich <contact@jochenmehlich.de>
2023-05-03 10:51:28 +0200

    improved timer -> minutes, stable formatting

--- server/src/assets/game/script-timer.js
+++ server/src/assets/game/script-timer.js
@@ -1,9 +1,17 @@
 var sec = 0;
+var min = 0;
 var stop = 1;
 function add(){
     if(stop == 0){
         sec++;
-        document.getElementById("timer").innerHTML = sec;
+        if (sec == 60){
+            sec = 0;
+            min++;
+        }
+        _min = ('0' + min.toString()).substr( -2 );
+        _sec = ('0' + sec.toString()).substr( -2 );
+        $("#timerSecond").html(_sec);
+        $("#timerMinute").html(_min);
         timer();
     }
 }
@@ -17,5 +25,5 @@ function starttimer(){
 function stoptimer(){
     stop = 1;
     sec = 0;
+    min = 0;
 }
-
