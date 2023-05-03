7682408b81fb9755adc0900b9508b86f311d2444
Jochen Mehlich <contact@jochenmehlich.de>
2023-05-03 10:31:13 +0200

    added timer

---
+++ server/src/assets/game/script-timer.js
@@ -0,0 +1,28 @@
+var sec = 0;
+var stop = 1;
+function add(){
+    if(stop == 0){
+        sec++;
+        document.getElementById("timer").innerHTML = seconds;
+        timer();
+    }
+}
+function timer(){
+    t = setTimeout(add, 1000);
+}
+function starttimer(){
+    stop = 0;
+    timer();
+}
+function stoptimer(){
+    stop = 1;
+    sec = 0;
+}
+
+$("#speedForm").on("change", function(){
+    if($("#speedForm").is(":visible")){
+        starttimer();
+    }else{
+        stoptimer();
+    }
+});
