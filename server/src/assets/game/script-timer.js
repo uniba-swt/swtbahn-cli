efe9a104805ce7888d9aa3bc52fcd884904ddce6
Jochen Mehlich <contact@jochenmehlich.de>
2023-05-05 13:35:30 +0200

    changed timer to proper class with interval triggered mechanic

--- server/src/assets/game/script-timer.js
+++ server/src/assets/game/script-timer.js
@@ -1,29 +1,28 @@
-var sec = 0;
-var min = 0;
-var stop = 1;
-function add(){
-    if(stop == 0){
-        sec++;
-        if (sec == 60){
-            sec = 0;
-            min++;
-        }
-        _min = ('0' + min.toString()).substr( -2 );
-        _sec = ('0' + sec.toString()).substr( -2 );
-        $("#timerSecond").html(_sec);
-        $("#timerMinute").html(_min);
-        timer();
-    }
-}
-function timer(){
-    t = setTimeout(add, 1000);
-}
-function starttimer(){
-    stop = 0;
-    timer();
-}
-function stoptimer(){
-    stop = 1;
+class Timer{
     sec = 0;
     min = 0;
+    stop = 1;
+
+    timerInvervall;
+    tick(){
+        if(stop == 0){
+            this.sec++;
+            if (this.sec == 60){
+                this.sec = 0;
+                this.min++;
+            }
+            _min = ('0' + this.min.toString()).substr( -2 );
+            _sec = ('0' + this.sec.toString()).substr( -2 );
+            $("#timerSecond").html(_sec);
+            $("#timerMinute").html(_min);
+        }
+    }
+    start(){
+        this.timerInvervall = setInterval(tick(), 1000);
+    }
+    stop(){
+        clearInterval(this.timerInvervall);
+        this.sec = 0;
+        this.min = 0;
+    }
 }
