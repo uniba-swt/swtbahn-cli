e828f3f0c00304e5ebf5de920e1d7ec94d4bcbb4
Bernhard Luedtke <bernhard_luedtke@posteo.de>
2023-05-22 10:00:37 +0200

    made timer js formatting consistent

--- server/src/assets/game/script-timer.js
+++ server/src/assets/game/script-timer.js
@@ -1,30 +1,29 @@
-class Timer{
-    sec = 0;
-    min = 0;
-    timerInterval;
+class Timer {
+	sec = 0;
+	min = 0;
+	timerInterval;
 
-    tick(){
-        this.sec++;
-        if (this.sec == 60){
-	        this.sec = 0;
-        	this.min++;
+	tick() {
+		this.sec++;
+		if (this.sec == 60) {
+			this.sec = 0;
+			this.min++;
+		}
+		var _min = ('0' + this.min.toString()).substr(-2);
+		var _sec = ('0' + this.sec.toString()).substr(-2);
+		$("#timerSecond").html(_sec);
+		$("#timerMinute").html(_min);
 	}
-    	var _min = ('0' + this.min.toString()).substr(-2);
-    	var _sec = ('0' + this.sec.toString()).substr(-2);
-    	$("#timerSecond").html(_sec);
-    	$("#timerMinute").html(_min);
-	console.log("Tick end");
-    }
 
+	start() {
+		this.sec = 0;
+		this.min = 0;
+		this.timerInterval = setInterval(() => { this.tick(); }, 1000);
+	}
 
-    start(){
-	this.sec = 0;
-	this.min = 0;
-        this.timerInterval = setInterval(() => {this.tick();}, 1000);
-    }
-    stop(){
-        clearInterval(this.timerInterval);
-        this.sec = 0;
-        this.min = 0;
-    }
+	stop() {
+		clearInterval(this.timerInterval);
+		this.sec = 0;
+		this.min = 0;
+	}
 }
