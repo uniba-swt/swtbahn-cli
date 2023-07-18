8f64205e689d74bb86704dee7460b6b512a48388
Bernhard Luedtke <bernhard_luedtke@posteo.de>
2023-07-18 16:01:10 +0200

    free yaml scalar on parsing iteration

--- server/src/parsers/parser_util.c
+++ server/src/parsers/parser_util.c
@@ -140,7 +140,9 @@ void parse_yaml_content(yaml_parser_t *parser,
                 error = true;
                 break;
         }
-
+        if (last_scalar != NULL) {
+            free(last_scalar);
+        }
         last_scalar = cur_scalar;
     } while(!error);
 }
