8a1088aa57b0865a73d00135ca121546ef5a6df5
Bernhard Luedtke <bernhard_luedtke@posteo.de>
2023-11-28 15:23:59 +0100

    engine uploader basic log refactor

--- server/src/websocket_uploader/engine_uploader.c
+++ server/src/websocket_uploader/engine_uploader.c
@@ -110,9 +110,8 @@ bool parse_model_into_verif_msg_str(GString *destination, const char *model_file
  */
 void send_verif_req_message(struct mg_connection *ws_connection, ws_verif_data* ws_data_ptr) {
 	if (ws_connection == NULL || ws_data_ptr == NULL) {
-		syslog_server(LOG_ERR, 
-		              "Websocket upload engine: Unable to open and parse model file, "
-		              "connection or ws_verif_data* is/are NULL");
+		syslog_server(LOG_ERR, "Send verification request message - invalid parameters");
+		return;
 	}
 	
 	// Read sctx model from file, build msg with necessary formatting
@@ -121,8 +120,7 @@ void send_verif_req_message(struct mg_connection *ws_connection, ws_verif_data* 
 	
 	// Check if parsing succeeded
 	if (!parse_success) {
-		syslog_server(LOG_ERR, 
-		              "Websocket upload engine: Unable to open and parse model file to be uploaded");
+		syslog_server(LOG_ERR,  "Send verification request message -  unable to parse model file");
 		ws_data_ptr->finished = true;
 		ws_data_ptr->success = false;
 		g_string_free(g_verif_msg_str, true);
@@ -132,18 +130,18 @@ void send_verif_req_message(struct mg_connection *ws_connection, ws_verif_data* 
 	// Try to send verification request
 	if (g_verif_msg_str != NULL && g_verif_msg_str->len > 0) {
 		syslog_server(LOG_INFO, 
-		              "Websocket upload engine: Sending verification request to verifier server");
+		              "Send verification request message - sending request to verifier server");
 		ssize_t sent_bytes = mg_ws_send(ws_connection, g_verif_msg_str->str, 
 		                                g_verif_msg_str->len, WEBSOCKET_OP_TEXT);
 		if (sent_bytes <= 0) {
 			syslog_server(LOG_ERR, 
-			              "Websocket upload engine: Sending verification request failed");
+			              "Send verification request message - sending verification request failed");
 			ws_data_ptr->finished = true;
 			ws_data_ptr->success = false;
 		}
 	} else {
 		syslog_server(LOG_ERR, 
-		              "Websocket upload engine: Websocket msg payload setup has gone wrong");
+		              "Send verification request message - failed to construct message to send");
 		ws_data_ptr->finished = true;
 		ws_data_ptr->success = false;
 	}
@@ -161,8 +159,7 @@ void send_verif_req_message(struct mg_connection *ws_connection, ws_verif_data* 
  */
 void process_verification_result_msg(struct mg_ws_message *ws_msg, ws_verif_data *ws_data_ptr) {
 	if (ws_msg == NULL || ws_data_ptr == NULL) {
-		syslog_server(LOG_ERR, 
-		              "Websocket upload engine: the received message or ws_verif_data is NULL");
+		syslog_server(LOG_ERR, "Process verification result message - invalid parameters");
 		return;
 	}
 	
@@ -171,7 +168,7 @@ void process_verification_result_msg(struct mg_ws_message *ws_msg, ws_verif_data
 	
 	if (verif_success) {
 		syslog_server(LOG_INFO, 
-		              "Websocket upload engine: Verification Server done, verification succeeded");
+		              "Process verification result message - engine satisfies all its properties");
 		ws_data_ptr->success = true;
 		ws_data_ptr->finished = true;
 	} else {
@@ -181,14 +178,14 @@ void process_verification_result_msg(struct mg_ws_message *ws_msg, ws_verif_data
 		if (!status_false_in_reply) {
 			// No 'status' field in answer with either true or false
 			syslog_server(LOG_INFO, 
-			              "Websocket upload engine: Verification Server done,"
-			              " verification status not specified");
+			              "Process verification result message - "
+			              "engine verification result inconclusive");
 			ws_data_ptr->message  = g_string_new("Verification done but no result status known.");
 		} else {
 			// Ordinary failure. Save server's reply (to forward to client later on)
 			syslog_server(LOG_INFO, 
-			              "Websocket upload engine: Verification Server done, "
-			              "verification did not succeed");
+			              "Process verification result message - "
+			              "engine does not satisfy all its properties");
 			ws_data_ptr->message  = g_string_new("");
 			g_string_append_printf(ws_data_ptr->message,"%s", ws_msg->data.ptr);
 		}
@@ -209,16 +206,15 @@ void process_verif_server_reply(struct mg_ws_message *ws_msg, ws_verif_data *ws_
 	// Parses mg_ws_message, which is expected to contain a message from the verification server.
 	// Then adjusts ws_data_ptr struct according to server's message.
 	if (ws_msg == NULL || ws_data_ptr == NULL) {
-		syslog_server(LOG_INFO, 
-		              "Websocket upload engine: Can't process verification server reply, "
-		              "invalid parameters");
+		syslog_server(LOG_ERR, "Process verification server reply - invalid parameters");
+		return;
 	}
 	
 	// Check that expected field "__MESSAGE_TYPE__" is contained in message
 	char *msg_type_is_defined = strstr(ws_msg->data.ptr, msg_type_field_key);
 	if (!msg_type_is_defined) {
-		syslog_server(LOG_INFO, 
-		              "Websocket upload engine: Verification Server replied in unknown format");
+		syslog_server(LOG_ERR, 
+		              "Process verification server reply - reply lacks __MESSAGE_TYPE__ field");
 		return;
 	}
 	
@@ -229,16 +225,17 @@ void process_verif_server_reply(struct mg_ws_message *ws_msg, ws_verif_data *ws_
 	if (type_verif_req_received) {
 		// verification has started
 		syslog_server(LOG_INFO, 
-		              "Websocket upload engine: Verification Server started verification");
+		              "Process verification server reply - verification server begun verification");
 		ws_data_ptr->started = true;
 	} else if (type_verif_req_result) {
+		syslog_server(LOG_INFO, 
+		              "Process verification server reply - verification server completed verification");
 		// verification has finished, parse result (updates ws_data_ptr)
 		process_verification_result_msg(ws_msg, ws_data_ptr);
 	} else {
 		// Unknown message type specified by the server.
 		syslog_server(LOG_WARNING, 
-		              "Websocket upload engine: Verification Server replied "
-		              "in unknown format");
+		              "Process verification server reply - invalid reply format");
 		// We are pessimistic and assume that the verification server will not reply again
 		// after this "mistake"
 		ws_data_ptr->success = false;
@@ -262,14 +259,13 @@ void websocket_verification_callback(struct mg_connection *ws_connection,
                                      int ev, void *ev_data, void *fn_data) {
 	ws_verif_data *ws_data_ptr = fn_data;
 	if (ws_connection == NULL) {
-		syslog_server(LOG_ERR, 
-		              "Websocket upload engine: ws_connection is NULL in Websocket callback");
+		syslog_server(LOG_ERR, "Websocket verification callback - connection is NULL");
 		// Error is encountered, the verification can't be completed 
 		ws_data_ptr->success = false;
 		ws_data_ptr->finished = true;
 	} else if (ev == MG_EV_ERROR) {
 		syslog_server(LOG_ERR, 
-		              "Websocket upload engine: Websocket callback received error event: %s", 
+		              "Websocket verification callback - received error event: %s", 
 		              (char *) ev_data);
 		// Error is encountered, the verification can't be completed 
 		ws_data_ptr->success = false;
@@ -282,7 +278,8 @@ void websocket_verification_callback(struct mg_connection *ws_connection,
 		process_verif_server_reply((struct mg_ws_message *) ev_data, ws_data_ptr);
 	} else if (ev == MG_EV_CLOSE) {
 		syslog_server(LOG_INFO, 
-		              "Websocket upload engine: Closing websocket connection to verifier server");
+		              "Websocket verification callback - "
+		              "closing websocket connection to verifier server");
 		ws_data_ptr->finished = true;
 	}
 }
@@ -293,11 +290,10 @@ verif_result verify_engine_model(const char* f_filepath) {
 	ws_verif_data ws_verif_data = {false, false, false, g_string_new(f_filepath), NULL};
 	
 	if (verifier_url == NULL) {
-		syslog_server(LOG_ERR, 
-		              "Websocket upload engine: No verifier URL has been set, abort");
+		syslog_server(LOG_ERR, "Verify engine model - no verifier URL has been set, abort");
 		verif_result result_data;
 		result_data.success = false;
-		result_data.message = g_string_new("No verifier URL has been set, "
+		result_data.message = g_string_new("No verifier server URL has been set, "
 		                                   "thus no verification was possible");
 		return result_data;
 	}
@@ -322,7 +318,7 @@ verif_result verify_engine_model(const char* f_filepath) {
 			ws_verif_data.finished = true;
 			ws_verif_data.success = false;
 			syslog_server(LOG_WARNING, 
-			              "Websocket upload engine: Verification did not start within %d ms, abort", 
+			              "Verify engine model - verification did not start within %d ms, abort", 
 			              (poll_counter * websocket_single_poll_length_ms));
 		}
 	}
@@ -394,15 +390,14 @@ void load_cached_verifier_url() {
 			
 			free(buffer);
 			syslog_server(LOG_INFO, 
-			              "Websocket load cached verifier url: loaded %s from cache", 
+			              "Load cached verifier URL - loaded URL %s from cache", 
 			              verifier_url);
 		} else {
-			syslog_server(LOG_NOTICE, 
-			              "Websocket load cached verifier url: no content in cache file");
+			syslog_server(LOG_NOTICE,  "Load cached verifier URL - no content in cache file");
 		}
 	} else {
 		syslog_server(LOG_NOTICE, 
-		              "Websocket load cached verifier url: url cache file could not be opened");
+		              "Load cached verifier URL - cache file could not be opened");
 	}
 }
 
@@ -410,20 +405,20 @@ void load_cached_verifier_url() {
 void cache_verifier_url() {
 	// write current verifier url to cache file unless url is null
 	if (verifier_url == NULL) {
-		syslog_server(LOG_NOTICE, "Websocket cache verifier url: url not cached as it is NULL");
+		syslog_server(LOG_NOTICE, "Cache verifier URL - not cached as URL is NULL");
 		return;
 	}
 	
 	// Open file, write string into file, close file.
 	FILE* file = fopen(cache_file_verifier_url, "w");
 	if (file == NULL) {
-		syslog_server(LOG_ERR, "Websocket cache verifier url: file opening failed");
+		syslog_server(LOG_ERR, "Cache verifier URL - cache file opening failed");
 		return;
 	}
 
 	// Write the content to the file
 	fputs(verifier_url, file);
-	syslog_server(LOG_INFO, "Websocket cache verifier url: cached url %s", verifier_url);
+	syslog_server(LOG_INFO, "Cache verifier URL - cached url %s", verifier_url);
 	
 	// Close the file
 	fclose(file);
