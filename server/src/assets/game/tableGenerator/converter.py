b1dbbf3c5a63e6286a134d0a31eaddd3f7aa7b2a
Jochen Mehlich <contact@jochenmehlich.de>
2024-01-24 13:04:38 +0100

    changed table generator (converter) for global usage with all generation file
    Signed-off-by: Jochen Mehlich <contact@jochenmehlich.de>

--- server/src/assets/game/tableGenerator/converter.py
+++ server/src/assets/game/tableGenerator/converter.py
@@ -1,129 +1,158 @@
 from csv import reader
-import json
-import yaml
+import json, yaml, os
 
-interlockingTableFile = "../../../../../configurations/swtbahn-standard/interlocking_table.yml"
-configuratonBahnFile = "../../../../../configurations/swtbahn-standard/extras_config.yml"
+pathToConfig = "../../../../../configurations"
+
+
+
+interlockingTableFileName = "interlocking_table.yml"
+configurationBahnFileName = "extras_config.yml"
+
 blacklistFile = "./blacklist.txt"
-groupingFile = "./flags-swtbahn-standard.csv"
-interlockingTable = json.loads(json.dumps(yaml.safe_load(open(interlockingTableFile))))
-configuratonBahn = json.loads(json.dumps(yaml.safe_load(open(configuratonBahnFile))))
+groupingFileDirectory = "./flagMappings"
 
-blacklist = []
+configFolderItemList = os.scandir(pathToConfig)
+folderList = []
 
-for line in open(blacklistFile):
-    blacklist.append(int(line.strip()))
+for entry in configFolderItemList:
+    if entry.is_dir():
+        folderList.append(entry.name)
 
-resultData = {}
+for configuration in folderList:
+    
+    mappingFolderContent = os.scandir(groupingFileDirectory)
+    flagMappings = []
+    for entry in mappingFolderContent:
+        if entry.is_file() and entry.name[-4:] == ".csv":
+            flagMappings.append(entry.name[:-4])
 
-blocktypes = ["blocks", "platforms"]
+    if configuration not in flagMappings:
+        break
 
-for blockType in blocktypes:
-    for block in configuratonBahn[blockType]:
-        try:
-            signals = block["signals"]
-        except KeyError:
-#            print("Block {} has no signals".format(block["id"]))
-            signals = []
-        routes = []
-        for signal in signals:
-            destinations = []
-            for route in interlockingTable["interlocking-table"]:
-                if route["source"] == signal:
-                    if route["destination"] not in destinations:
-                        destinations.append(route["destination"])
-            for destination in destinations:
-                routeID = -1
+    interlockingTableFile = "{}/{}/{}".format(pathToConfig, configuration, interlockingTableFileName)
+    configurationBahnFile = "{}/{}/{}".format(pathToConfig, configuration, configurationBahnFileName)
+
+
+    interlockingTable = json.loads(json.dumps(yaml.safe_load(open(interlockingTableFile))))
+    configuratonBahn = json.loads(json.dumps(yaml.safe_load(open(configurationBahnFile))))
+    
+    groupingFile = "{}/{}.csv".format(groupingFileDirectory, configuration)
+
+    blacklist = []
+
+    for line in open(blacklistFile):
+        blacklist.append(int(line.strip()))
+
+    resultData = {}
+
+    blocktypes = ["blocks", "platforms"]
+
+    for blockType in blocktypes:
+        for block in configuratonBahn[blockType]:
+            try:
+                signals = block["signals"]
+            except KeyError:
+    #            print("Block {} has no signals".format(block["id"]))
+                signals = []
+            routes = []
+            for signal in signals:
+                destinations = []
                 for route in interlockingTable["interlocking-table"]:
-                    if route["source"] == signal and route["destination"] == destination:
-                        if routeID == -1:
-                            routeID = route["id"]
-                            print("{} --> {} | {} {}".format(signal, destination, routeID, block["id"]))                
-                        else:
-                            if interlockingTable["interlocking-table"][routeID]["id"] in blacklist:
-                                print("Route was overriden because old was blacklisted")
-                            elif interlockingTable["interlocking-table"][route["id"]]["id"] in blacklist:
-                                 print("Route Beibehalten")
-                            elif len(interlockingTable["interlocking-table"][route["id"]]["path"]) < len(interlockingTable["interlocking-table"][routeID]["path"]):
+                    if route["source"] == signal:
+                        if route["destination"] not in destinations:
+                            destinations.append(route["destination"])
+                for destination in destinations:
+                    routeID = -1
+                    for route in interlockingTable["interlocking-table"]:
+                        if route["source"] == signal and route["destination"] == destination:
+                            if routeID == -1:
                                 routeID = route["id"]
-                                print("{} -> {} | {} {}".format(signal, destination, routeID, block["id"]))
-                
+                                print("{} --> {} | {} {}".format(signal, destination, routeID, block["id"]))                
+                            else:
+                                if interlockingTable["interlocking-table"][routeID]["id"] in blacklist:
+                                    print("Route was overriden because old was blacklisted")
+                                elif interlockingTable["interlocking-table"][route["id"]]["id"] in blacklist:
+                                    print("Route Beibehalten")
+                                elif len(interlockingTable["interlocking-table"][route["id"]]["path"]) < len(interlockingTable["interlocking-table"][routeID]["path"]):
+                                    routeID = route["id"]
+                                    print("{} -> {} | {} {}".format(signal, destination, routeID, block["id"]))
+                    
 
-                if routeID not in blacklist:
-                    routes.append(routeID)
+                    if routeID not in blacklist:
+                        routes.append(routeID)
+                    else:
+                        print("Route {} wurde ignoriert, weil diese in der Blacklist steht".format(routeID))
+
+            resultData[block["id"]] = {}
+            for route in routes:
+                destination = interlockingTable["interlocking-table"][route]["destination"]
+                routeID = route
+                orientation = interlockingTable["interlocking-table"][route]["orientation"]
+                lastBlock = interlockingTable["interlocking-table"][route]["sections"][-1]["id"]
+                stopSegment = None
+                segments = []
+                isError = False
+
+                for qblock in configuratonBahn["platforms"]:
+                    if qblock["id"] == lastBlock:
+                        for segment in qblock["main"]:
+                            segments.append(segment)
+                        break
+                for qblock in configuratonBahn["blocks"]:
+                    if qblock["id"] == lastBlock:
+                        for segment in qblock["main"]:
+                            segments.append(segment)
+                        break
+                if len(segments) == 1:
+    #                print("Segment: {} at Block {}".format(segments[0], lastBlock))
+                    stopSegment = segments[0]
+                elif len(segments) == 2:
+                    if segments[0][0:-1] == segments[1][0:-1]:
+    #                   print(segments)
+                        stopSegment = segments[0][0:-1]
+    #                    print(stopSegment)
+                    else:
+                        print("Error Segment {} and Segment {} are not the same. Block {}".format(segments[0], segments[1], block["id"]))
+                        isError = True
                 else:
-                    print("Route {} wurde ignoriert, weil diese in der Blacklist steht".format(routeID))
-
-        resultData[block["id"]] = {}
-        for route in routes:
-            destination = interlockingTable["interlocking-table"][route]["destination"]
-            routeID = route
-            orientation = interlockingTable["interlocking-table"][route]["orientation"]
-            lastBlock = interlockingTable["interlocking-table"][route]["sections"][-1]["id"]
-            stopSegment = None
-            segments = []
-            isError = False
-
-            for qblock in configuratonBahn["platforms"]:
-                if qblock["id"] == lastBlock:
-                    for segment in qblock["main"]:
-                        segments.append(segment)
-                    break
-            for qblock in configuratonBahn["blocks"]:
-                if qblock["id"] == lastBlock:
-                    for segment in qblock["main"]:
-                        segments.append(segment)
-                    break
-            if len(segments) == 1:
-#                print("Segment: {} at Block {}".format(segments[0], lastBlock))
-                stopSegment = segments[0]
-            elif len(segments) == 2:
-                if segments[0][0:-1] == segments[1][0:-1]:
- #                   print(segments)
-                    stopSegment = segments[0][0:-1]
-#                    print(stopSegment)
-                else:
-                    print("Error Segment {} and Segment {} are not the same. Block {}".format(segments[0], segments[1], block["id"]))
+                    print("Error with BlockID {}".format(block["id"]))
                     isError = True
-            else:
-                print("Error with BlockID {}".format(block["id"]))
-                isError = True
-            if isError:
-                print("error with {}".format(route))
-                break
-            resultData[block["id"]][destination] = {}
-            resultData[block["id"]][destination]["route-id"] = routeID
-            resultData[block["id"]][destination]["orientation"] = orientation
-            resultData[block["id"]][destination]["block"] = lastBlock
-            resultData[block["id"]][destination]["segment"] = stopSegment
+                if isError:
+                    print("error with {}".format(route))
+                    break
+                resultData[block["id"]][destination] = {}
+                resultData[block["id"]][destination]["route-id"] = routeID
+                resultData[block["id"]][destination]["orientation"] = orientation
+                resultData[block["id"]][destination]["block"] = lastBlock
+                resultData[block["id"]][destination]["segment"] = stopSegment
 
-# Sort
-originalResultData = resultData
-resultData = {}
-for block in originalResultData:
-    destinations = []
-    for destination in originalResultData[block]:
-        destinations.append(destination)
-    destinationsSorted = []
-    with open(groupingFile, "r") as csvFile:
-        csv_reader = reader(csvFile)
-        for row in csv_reader:
-            signal = "signal" + row[0]
-            print(signal)
-            if signal in destinations:
-                destinationsSorted.append(signal)
-            signal = signal + "a"
-            if signal in destinations:
-                destinationsSorted.append(signal)
+    # Sort
+    originalResultData = resultData
+    resultData = {}
+    for block in originalResultData:
+        destinations = []
+        for destination in originalResultData[block]:
+            destinations.append(destination)
+        destinationsSorted = []
+        with open(groupingFile, "r") as csvFile:
+            csv_reader = reader(csvFile)
+            for row in csv_reader:
+                signal = "signal" + row[0]
+                print(signal)
+                if signal in destinations:
+                    destinationsSorted.append(signal)
+                signal = signal + "a"
+                if signal in destinations:
+                    destinationsSorted.append(signal)
 
-    resultData[block] = {}
-    for destination in destinationsSorted:
-        resultData[block][destination] = {}
-        resultData[block][destination]["route-id"] = originalResultData[block][destination]["route-id"]
-        resultData[block][destination]["orientation"] = originalResultData[block][destination]["orientation"]
-        resultData[block][destination]["block"] = originalResultData[block][destination]["block"]
-        resultData[block][destination]["segment"] = originalResultData[block][destination]["segment"]
-with open("../destinations-swtbahn-standard.json", "w") as file:
-    file.write("const allPossibleDestinationsSwtbahnStandard = ")
-    file.write(json.dumps(resultData, indent=2))
-    file.write(";")
+        resultData[block] = {}
+        for destination in destinationsSorted:
+            resultData[block][destination] = {}
+            resultData[block][destination]["route-id"] = originalResultData[block][destination]["route-id"]
+            resultData[block][destination]["orientation"] = originalResultData[block][destination]["orientation"]
+            resultData[block][destination]["block"] = originalResultData[block][destination]["block"]
+            resultData[block][destination]["segment"] = originalResultData[block][destination]["segment"]
+    with open("../destinations-{}.json".format(configuration), "w") as file:
+        file.write("const allPossibleDestinations = ")
+        file.write(json.dumps(resultData, indent=2))
+        file.write(";")
--- server/src/assets/game/tableGenerator/generator.py
+++ server/src/assets/game/tableGenerator/generator.py
@@ -7,8 +7,10 @@ def numberToWord(number):
 def characterToColor(character):
 	return {"b": "Blue", "g": "Green", "r": "Red", "s": "Black"}[character]
 
+alias = input("What train is used (alias): ")
+filestring = input("Where is the Signal Mapping csv? ")
 # Load Match to Signal into File
-with open("flags-swtbahn-standard.csv", "r") as f:
+with open(filestring, "r") as f:
     reader = csv.reader(f)
     jsonString = {}
     for row in reader:
@@ -32,7 +34,7 @@ with open("flags-swtbahn-standard.csv", "r") as f:
         jsonString[signalName] = endString
 
     with open("../flags-swtbahn-standard.json", "w") as file:
-        file.write("const signalFlagMapSwtbahnStandard = ")
+        file.write("const signalFlagMap{} = ".format(alias))
         file.write(json.dumps(jsonString, indent=2))
         file.write(";")
 
