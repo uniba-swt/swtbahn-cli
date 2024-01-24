2ae62e4845457e17a9396a74b7775be247bb7ced
Jochen Mehlich <contact@jochenmehlich.de>
2024-01-24 13:10:47 +0100

    changed table generator (generator.py) for all use
    Signed-off-by: Jochen Mehlich <contact@jochenmehlich.de>

--- server/src/assets/game/tableGenerator/generator.py
+++ server/src/assets/game/tableGenerator/generator.py
@@ -1,5 +1,4 @@
-import json
-import csv
+import json, csv, os
 
 def numberToWord(number):
     return ["Zero", "One", "Two", "Three", "Four", "Five", "Six"][int(number)]
@@ -7,34 +6,36 @@ def numberToWord(number):
 def characterToColor(character):
 	return {"b": "Blue", "g": "Green", "r": "Red", "s": "Black"}[character]
 
-alias = input("What train is used (alias): ")
-filestring = input("Where is the Signal Mapping csv? ")
-# Load Match to Signal into File
-with open(filestring, "r") as f:
-    reader = csv.reader(f)
-    jsonString = {}
-    for row in reader:
-        signal = row[0]
-        colorDefinition = row[1]
-        number = colorDefinition[-1]
+groupingFileDirectory = "./flagMappings"
+    
+mappingFolderContent = os.scandir(groupingFileDirectory)
+for entry in mappingFolderContent:
+    if entry.is_file() and entry.name[-4:] == ".csv":
+        with open("{}/{}".format(groupingFileDirectory, entry.name), "r") as f:
+            reader = csv.reader(f)
+            jsonString = {}
+            for row in reader:
+                signal = row[0]
+                colorDefinition = row[1]
+                number = colorDefinition[-1]
 
-        endString = "flagTheme" + characterToColor(colorDefinition[0])
+                endString = "flagTheme" + characterToColor(colorDefinition[0])
 
-        if colorDefinition[1] == "w":
-            endString += " flagOutline "
-        else:
-            endString += " flagFilled "
+                if colorDefinition[1] == "w":
+                    endString += " flagOutline "
+                else:
+                    endString += " flagFilled "
 
-        if int(number) == 6:
-            number = 0
+                if int(number) == 6:
+                    number = 0
 
-        endString += "flag" + numberToWord(number)
-        print(endString)
-        signalName = "signal{}".format(str(signal))
-        jsonString[signalName] = endString
+                endString += "flag" + numberToWord(number)
+                print(endString)
+                signalName = "signal{}".format(str(signal))
+                jsonString[signalName] = endString
 
-    with open("../flags-swtbahn-standard.json", "w") as file:
-        file.write("const signalFlagMap{} = ".format(alias))
-        file.write(json.dumps(jsonString, indent=2))
-        file.write(";")
+            with open("../flags-{}.json".format(entry.name[:-4]), "w") as file:
+                file.write("const signalFlagMap = ")
+                file.write(json.dumps(jsonString, indent=2))
+                file.write(";")
 
