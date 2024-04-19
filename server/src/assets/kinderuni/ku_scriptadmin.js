d6e90b33628237fbdecd8a1f1f057afec7d940b5
Bernhard Luedtke <bernhard_luedtke@posteo.de>
2024-04-19 13:41:23 +0200

    adjusted default train info fields and reenabled monitoring for all trains

--- server/src/assets/kinderuni/ku_scriptadmin.js
+++ server/src/assets/kinderuni/ku_scriptadmin.js
@@ -145,8 +145,8 @@ function updateTrainStatePromise(train) {
 }
 
 function updateTrainStates() {
-	//const trains = ["cargo_db", "cargo_bayern", "cargo_green", "regional_odeg", "regional_brengdirect"];
-	const trains = ["cargo_db"];
+	const trains = ["cargo_db", "cargo_bayern", "cargo_green", "regional_odeg", "regional_brengdirect"];
+	//const trains = ["cargo_db"];
 	for(var tr of trains) {
 		updateTrainStatePromise(tr);
 	}
